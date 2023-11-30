4a79430770f5839a3b70cf4cd87e62628b31b9ad
Gil Cohen <gil3co@gmail.com>
2023-11-30 21:35:04 +0000

    Try to mock /forms

--- src/routes/page.test.ts
+++ src/routes/page.test.ts
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/svelte"
 
-import { expect, it } from "vitest";
+import { expect, it, vi } from "vitest";
 import Page from "./Component.svelte";
 
-/**
- * @vitest-environment jsdom
- */
+vi.mock('$app/forms', () => {
+    return {};
+});
+
 it('should work', () => {
     render(Page);
     expect(screen.getByDisplayValue(12)).toBeInTheDocument()
